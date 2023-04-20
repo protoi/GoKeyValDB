e25c83f45233aa41aa9b20d1be0890947a67bffb
protoi <protikdatta9@gmail.com>
2023-04-21 01:37:27 +0530

    client data is of structure 'msg_len%actual_message'

--- scripts/client.py
+++ scripts/client.py
@@ -4,22 +4,23 @@ import time
 HOST = 'localhost'
 PORT = 4000
 messages = [
-    "get mykey ",
-    "set something sdjkkdjf ",
-    "del mykey ",
-    "del ddfks fskdfsdkf dksjk fdjskjkf ",
-    "set fkdj ",
-    "set ",
-    "get dh djf dh hfhdfh ",
-    "del ",
+    "get mykey",
+    "set something sdjkkdjf",
+    "del mykey",
+    "del ddfks fskdfsdkf dksjk fdjskjkf",
+    "set fkdj",
+    "set",
+    "get dh djf dh hfhdfh",
+    "del",
 ]
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
     print("connection successful")
-    for message in messages:
+    for raw_msg in messages:
+        message = f'{len(raw_msg)}%{raw_msg}'
         print("message being send ---> ", message)
         s.sendall(message.encode())
-        data = s.recv(1024)
+        data = s.recv(200)
         print('Received:', data.decode())
         time.sleep(1)
