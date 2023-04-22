18c99c1ae195ea83c9dcfafd76583ff31b485212
protoi <protikdatta9@gmail.com>
2023-04-23 03:23:12 +0530

    added a simple bidirectional string linked list implementation, will add min+max heaps and sortedsets, sets later

---
+++ linked_list.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"strings"
+)
+
+type Node struct {
+	data     string
+	previous *Node
+	next     *Node
+}
+
+type BiDirectionalLinkedList struct {
+	head *Node
+	tail *Node
+}
+
+func (list *BiDirectionalLinkedList) PushFront(data string) {
+	newNode := &Node{data: data, next: list.head}
+	if list.head != nil {
+		list.head.previous = newNode
+	}
+	list.head = newNode
+	if list.tail == nil {
+		list.tail = newNode
+	}
+}
+
+func (list *BiDirectionalLinkedList) PushBack(data string) {
+	newNode := &Node{data: data, previous: list.tail}
+	if list.tail != nil {
+		list.tail.next = newNode
+	}
+	list.tail = newNode
+	if list.head == nil {
+		list.head = newNode
+	}
+}
+
+func (list *BiDirectionalLinkedList) PopFront() (string, bool) {
+	if list.head == nil {
+		return "", false
+	}
+	data := list.head.data
+	list.head = list.head.next
+	if list.head != nil {
+		list.head.previous = nil
+	} else {
+		list.tail = nil
+	}
+	return data, true
+}
+
+func (list *BiDirectionalLinkedList) PopBack() (string, bool) {
+	if list.tail == nil {
+		return "", false
+	}
+	data := list.tail.data
+	list.tail = list.tail.previous
+	if list.tail != nil {
+		list.tail.next = nil
+	} else {
+		list.head = nil
+	}
+	return data, true
+}
+
+func (list *BiDirectionalLinkedList) getListFront() string {
+	if list.head == nil {
+		return "[]"
+	}
+
+	listElements := make([]string, 0)
+
+	current := list.head
+	for current != nil {
+		listElements = append(listElements, current.data)
+		current = current.next
+	}
+	listElements = append(listElements, "nil")
+
+	return "[" + strings.Join(listElements, ", ") + "]"
+
+}
+
+func (list *BiDirectionalLinkedList) getListBack() string {
+	if list.head == nil {
+		return "[]"
+	}
+
+	listElements := make([]string, 0)
+
+	current := list.tail
+	for current != nil {
+		//fmt.Printf("%v -> ", current.data)
+		listElements = append(listElements, current.data)
+		current = current.previous
+	}
+	listElements = append(listElements, "nil")
+
+	return "[" + strings.Join(listElements, ", ") + "]"
+}
